c57852e44b4dd0a9c8f62899cf61cfdc1461d706
Luis García <aig.luis@gmail.com>
2020-12-19 18:58:37 -0600

    Realizada peticion a la Api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
+  //state de la app
+  const [busqueda, guardarBusqueda] = useState("");
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      if (busqueda === "") return;
+      const imagenesPorPagina = 30;
+      const key = "0"; //la key aquí
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarBusqueda(resultado.hits);
+    };
+    consultarApi();
+  }, [busqueda]);
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imagenes</p>
+        <Formulario guardarBusqueda={guardarBusqueda} />
       </div>
     </div>
   );
---
+++ src/components/ListadoImagenes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListadoImagenes = () => {
+  return <h1>ListadoImagenes</h1>;
+};
+
+export default ListadoImagenes;
